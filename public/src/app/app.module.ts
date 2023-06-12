6a7be8b7025b6d4245630d0f9347aceac10b91a1
mnodson <87267104+mnodson@users.noreply.github.com>
2023-06-12 18:12:03 +0100

    add google auth (#3)
    * add google auth
    
    * update budgets
    
    * reorder login logic

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -6,19 +6,43 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
 import { environment } from '../environments/environment';
+import { AuthService } from './shared/services/auth.service';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './shared/guard/auth.guard';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

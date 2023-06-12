68e65281da0f2462b305c1e2c1fde226612427ce
Mark Nodson <mark.nodson@gmail.com>
2023-06-12 13:48:20 +0100

    firebase config

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ public/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyANgWYWRiDKguc2EqGD59D8409uqP4IaXs",
+        authDomain: "study-tool-3ebea.firebaseapp.com",
+        projectId: "study-tool-3ebea",
+        storageBucket: "study-tool-3ebea.appspot.com",
+        messagingSenderId: "180669484739",
+        appId: "1:180669484739:web:c55c693b92780ba3569c76",
+        measurementId: "G-CEMTL2KYE5"
+    }
+};
